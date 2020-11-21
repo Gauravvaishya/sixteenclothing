652fd1ca4636d0cec40184d21e172bb6cd1bef31
Gauravvaishya <74814506+Gauravvaishya@users.noreply.github.com>
2020-11-21 14:50:17 +0530

    Add files via upload

---
+++ jav.js
@@ -0,0 +1,18 @@
+
+       function load(){
+        var a;
+        a=document.getElementById('loader');
+        a.style.display='none';
+       }
+
+
+       // window.onscroll=function(){
+       // 	navcolor();
+       // }
+       // function navcolor(){
+       // 	if(document.body.scrollTop>70 || document.documentElement.scrollTop>70){
+       // 		document.getElementById('nvbr').style.background-color='white';
+       // 		document.getElementById('logo5').style.color='black';
+       // 	}
+       // }
+    
